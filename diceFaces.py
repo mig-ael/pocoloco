fc68cea9dda14dff33034c8eaf24b53ca5c041f5
mig-ael <dupreezmigael@gmail.com>
2025-05-13 13:56:59 -0700

    Started pocoloco created new file to get dice faces and started gameloop

---
+++ diceFaces.py
@@ -0,0 +1,48 @@
+def diceFaceOne():
+    print("+-------+",end='\t')
+    print("|       |",end='\t')
+    print("|   O   |",end='\t')
+    print("|       |",end='\t')
+    print("+-------+",end='\t')
+def diceFaceTwo():
+    print("+-------+",end='\t')
+    print("| O     |",end='\t')
+    print("|       |",end='\t')
+    print("|     O |",end='\t')
+    print("+-------+",end='\t')
+
+def diceFaceThree():
+    print("+-------+",end='\t'*3)
+    print("| O     |",end='\t')
+    print("|   O   |",end='\t')
+    print("|     O |",end='\t')
+    print("+-------+",end='\t')
+
+def diceFaceFour():
+    print("+-------+",end='\t')
+    print("| O   O |",end='\t')
+    print("|       |",end='\t')
+    print("| O   O |",end='\t')
+    print("+-------+",end='\t')
+
+def diceFaceFive():
+    print("+-------+",end='\t')
+    print("| O   O |",end='\t')
+    print("|   O   |",end='\t')
+    print("| O   O |",end='\t')
+    print("+-------+",end='\t')
+
+def diceFaceSix():
+    print("+-------+",end='\t')
+    print("| O   O |",end='\t')
+    print("| O   O |",end='\t')
+    print("| O   O |",end='\t')
+    print("+-------+",end='\t')
+
+diceTB1= "+-------+\t"
+dice12=  "|       |\t"
+dice232= "| O     |\t"
+dice4562="| O   O |\t"
+dice1353="|   O   |\t"
+playerRoll=4 #temp change the number to messar ound
+print(diceTB*3)
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-#Migael du Preez
+#Migael du Preez, Andrew Kim, and Haruki sat with us did nothing
+import random,diceFaces
+intro = input('Welcome to PocoLoco!\nWould you like to read the instructions? (y/n) ')
+if intro.capitalize()=='Y':
+    #print instrutions here
+    print('hi')
 
+name = input('What is your name? ')
+chipStart = input('How many chips do you want everyone to start with? ')
+chips={'PC1':chipStart, 'PC2':chipStart, 'PC3':chipStart}
+chips[name] = 10
+
+while True:
+    rollValues ={'1':100, '2':2, '3':3, '4':4, '5': 5, '6':60}
+    
+    playerRoll1=[]
+    playerRoll1.append(random.randint(1,6))*3
+    print(playerRoll1)
