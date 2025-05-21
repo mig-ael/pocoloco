c30e4b444b4e5100d2f57e253cce63e50fd8e9d9
AndrewK1210 <125704024+AndrewK1210@users.noreply.github.com>
2025-05-20 22:07:17 -0700

    fixed scorecard

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
-a={'test':3,'test2':4,'test4':5}
-a_list=[]
-for item in a:
-    a_list.append(item)
-
-
-for i in a_list:
-    item = a[i]
-    print(a_list[item])
-
 # search_value = 3
 
 # for key, value in a.items():
 #     if value == search_value:
 #         print(f" {search_value} is {key}")
+
+chips={'PC1':10, 'PC2':10, 'PC3':10}
+def chipUpdate(chips):
+    count=0
+    chips_list=[]
+    for item in chips:
+        chips_list.append(item)
+    
+    for item in chips_list:
+        count+=(len(str(chips[item])))
+
+    return count
+
+print(chipUpdate(chips))
